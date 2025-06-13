3a753ce8318db433f147209f895a15b5818eb22c
Samruddhi Patodi <samruddhi.patodi@gmail.com>
2025-06-13 22:36:55 +0530

    Add MainApp.java entry point

---
+++ src/main/java/com/flipkartcatalog/MainApp.java
@@ -0,0 +1,7 @@
+package main.java.com.flipkartcatalog;
+
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println("Flipkart Catalog Deduplication App Running...");
+    }
+}
